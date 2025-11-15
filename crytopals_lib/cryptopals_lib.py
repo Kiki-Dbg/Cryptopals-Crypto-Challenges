b398153c860977197cfdb2a427fccf123339a5c5
Kiki-Dbg <ecn418@gmail.com>
2025-11-15 22:15:59 +1100

    updated cryptopals_lib

--- crytopals_lib/cryptopals_lib.py
+++ crytopals_lib/cryptopals_lib.py
@@ -1,3 +1,7 @@
 from byte_conversion import *
 
 from xor_functions import *
+
+from file_io import *
+
+from language_analysis import *
--- crytopals_lib/xor_functions.py
+++ crytopals_lib/xor_functions.py
@@ -1,6 +1,19 @@
 
+from language_analysis import *
+
 ########################################
 #### XOR related functions          ####
 ########################################
 def xor_bytes(a, b):
     return bytes([ x^y for (x,y) in zip(a, b)])
+
+def xor_brute(ciphertext):
+    candidates = []
+    for key_try in range(256):
+        plaintext = xor_bytes(ciphertext, bytes([key_try])*len(ciphertext))
+        score = eng_score(plaintext)
+        result = {  'key': bytes([key_try]),
+                    'score': score,
+                    'plaintext': plaintext }
+        candidates.append(result)
+    return sorted(candidates, key=lambda c: c['score'], reverse=True)[0]
