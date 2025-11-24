99016637e08fba94a58ebaa3a3c1ffb8e66867e6
Kiki-Dbg <ecn418@gmail.com>
2025-11-24 21:47:43 +1100

    refactored crypto_utils library

--- crypto_utils/__init__.py
+++ crypto_utils/__init__.py
@@ -26,32 +26,3 @@ from .utils import hamming_dist
 from .block_logic import pkcs7_unpad, pkcs7_pad
 
 from .aes import decrypt_ecb, encrypt_ecb, detect_ecb
-
-#from .aes import encrypt_ecb, decrypt_ecb, detect_ecb
-#from .xor import xor_bytes, xor_repeating_key
-#from .byte_encoding import (
-#    hex_to_bytes, bytes_to_hex, b64_to_bytes, bytes_to_b64
-#)
-
-#from .io_utils import get_file_lines, get_file_text
-
-
-# Define the __all__ variable
-#__all__ = ["module1", "module2"]
-
-# Import the submodules
-#from . import module1
-#from . import module2
-
-
-
-
-#from aes import *
-
-#from byte_encoding import *
-
-#from lang_analysis import *
-
-#from io_utils import *
-
-#from xor import *
